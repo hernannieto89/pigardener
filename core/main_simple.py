6ea34aee61b7f945acb70f83a399d7e1953ceec4
hernan <hernan.nieto89@gmail.com>
2019-04-19 17:17:39 -0300

    fix import error

--- core/main_simple.py
+++ core/main_simple.py
@@ -2,22 +2,22 @@
 """
 Simple timer - Main module.
 """
-from .graceful_killer import GracefulKiller
-from .helpers import teardown, got_to_work, setup, work, check_sudo,\
+from core.graceful_killer import GracefulKiller
+from core.helpers import teardown, got_to_work, setup, work, check_sudo,\
     continuous_work, get_remaining_time, get_time_goal
 
 
-def simple_timer(timer_instance):
+def simple_timer(pins, start_time, end_time, work_time, sleep_time):
     """
     Simple timer.
     """
     check_sudo()
 
-    start = timer_instance.start_time
-    end = timer_instance.end_time
-    pins = list(timer_instance.pins)
-    work_time = timer_instance.work_time
-    sleep_time = timer_instance.sleep_time
+    start = start_time
+    end = end_time
+    pins = pins
+    work_time = work_time
+    sleep_time = sleep_time
     continuous = sleep_time <= 0
     # setup GPIO
     setup(pins)
